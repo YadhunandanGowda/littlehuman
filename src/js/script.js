61f6101ccaf94c11c608eae44ddfd606a5f7dce6
Yadhunandana Gowda B S <65999096+YadhunandanGowda@users.noreply.github.com>
2022-07-26 13:22:14 +0530

    most changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
   $("#banner-section").css("height", window.innerHeight + "px");
 });
+
+$(window).on("scroll", function (e) {
+  if (document.documentElement.scrollTop < 50) {
+    $("#header").removeClass("grayBG");
+  } else {
+    if (!$("#header").hasClass("grayBG")) {
+      $("#header").addClass("grayBG");
+    }
+  }
+});
