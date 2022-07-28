b31e9d82c7e5c956fcf7bef4312c35ddba569d62
Yadhunandana Gowda B S <65999096+YadhunandanGowda@users.noreply.github.com>
2022-07-28 22:11:00 +0530

    slider changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,35 @@
 $(document).ready(function () {
   $("#banner-section").css("height", window.innerHeight + "px");
+
+  $("#sliderPlay").slick({
+    centerMode: true,
+    dots: true,
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow:
+      "<button class='prev-arr sliderArr'><img src='./images/left-arrow.svg'/></button>",
+    nextArrow:
+      "<button class='next-arr sliderArr'><img src='./images/right-arrow.svg'/></button>",
+  });
+
+  $(".sessionSlider").slick({
+    centerMode: true,
+    dots: true,
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow:
+      "<button class='prev-arr sliderArr'><img src='./images/left-arrow.svg'/></button>",
+    nextArrow:
+      "<button class='next-arr sliderArr'><img src='./images/right-arrow.svg'/></button>",
+  });
 });
 
-$(window).on("scroll", function (e) {
-  if (document.documentElement.scrollTop < 50) {
-    $("#header").removeClass("grayBG");
-  } else {
-    if (!$("#header").hasClass("grayBG")) {
-      $("#header").addClass("grayBG");
-    }
-  }
-});
+// $(window).on("scroll", function (e) {
+//   if (document.documentElement.scrollTop < 50) {
+//     $("#header").removeClass("grayBG");
+//   } else {
+//     if (!$("#header").hasClass("grayBG")) {
+//       $("#header").addClass("grayBG");
+//     }
+//   }
+// });
